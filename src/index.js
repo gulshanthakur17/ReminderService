26f0c63147fc202c1c2e5ac2e33a405ac1d29b24
Gulshan Thakur <gulshanthakur205@gmail.com>
2023-09-30 21:49:42 +0530

    Added Message Queue

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/server-config');
 
-//const { sendBasicEmail } = require('./services/email-service');
 
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/message-queue');
+const { REMINDER_BINDING_KEY } = require('./config/server-config');
 
-const setupAndStartServer =  () => {
+const setupAndStartServer =  async() => {
 
     const app = express();
     app.use(bodyParser.json());
@@ -17,17 +19,15 @@ const setupAndStartServer =  () => {
 
     app.post('/api/v1/tickets', TicketController.create);
 
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents,REMINDER_BINDING_KEY);
+
+
     app.listen(PORT, () => {
         console.log(`Server Started At Port ${PORT}`);
 
-        jobs();
+        //jobs();
 
-        // sendBasicEmail (
-        //     'support@admin.com',
-        //     'demo@gmail.com',
-        //     'This is a testing email',
-        //     'Hey, how are you, I hope you like the support',
-        // );
     });
 
 }
